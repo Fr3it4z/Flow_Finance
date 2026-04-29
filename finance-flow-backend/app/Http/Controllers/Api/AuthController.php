e26e79f32d6351f357fb815b505c37f789edefe6
Fr3it4z <afonsobaratafreitas@gmail.com>
2026-04-29 10:06:40 +0100

    feat: API CRUD Categorias

--- finance-flow-backend/app/Http/Controllers/Api/AuthController.php
+++ finance-flow-backend/app/Http/Controllers/Api/AuthController.php
@@ -4,14 +4,68 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     //Funções para registar
     public function register(Request $request)
     {
+        //Validar os campos antes de aceita-los
         $scancamps = $request->validate([
             'name'=>'required|string|max:255',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|string|min:6'
+        ]);
+
+        //Criar o Utilizador na bd e faz o hash
+        $user = User::create([
+            'name'=>$scancamps['name'],
+            'email'=>$scancamps['email'],
+            'password'=>Hash::make($scancamps['password'])
+        ]);
+
+        //Criar o token para o user
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message'=> 'Conta criada com sucesso',
+            'token'=> $token
+        ],201);
+    }
+    public function login(Request $request) 
+    {
+        $scancamps = $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|string|min:6'
+        ]);
+
+        $user = User::where('email',$scancamps['email'])->first();
+
+        //Validação de campos da password
+        if(!$user || !Hash::check($scancamps['password'],$user -> password))
+        {
+            return response()->json(['message' => 'Credenciais incorretas'], 401);
+        }
+        
+        // Se tudo estiver bem, gera um novo Token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        //login sucesso
+        return response()->json([
+            'message' => 'Login efetuado com sucesso!',
+            'token' => $token
+        ]);
+    }
+    //Fazer lougout
+    public function logout(Request $request)
+    {
+        //search the token and remove
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logout efetuado com sucesso. Volte sempre!'
         ]);
     }
 }
--- finance-flow-backend/routes/api.php
+++ finance-flow-backend/routes/api.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+
+//Rotas Publicas
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+//Rotas Privadas
+Route::middleware('auth:sanctum')->group(function () {
+    //Rota de Logout
+    Route::post('/logout', [AuthController::class,'logout']);
+
+    //Outras rotas ficaram aqui
+
+    //Rotas de categorias
+    Route::apiResource('categories', CategoryController::class);
+    
+
+});
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
