a49b9a00c366e97127b7f4898aabb8b6c8b645c6
fr3it4z <afonsobaratafreitas@gmail.com>
2026-06-23 16:01:09 +0100

    Fix: Files Names

--- frontend/src/app/services/user.ts
+++ frontend/src/app/services/user.ts
@@ -9,9 +9,9 @@ export class UserService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:8000/api';
 
-  getProfile(): Observable<any> {
+/*   getProfile(): Observable<any> {
     // 🛑 O TEU CÓDIGO AQUI: 
     // Faz o return de um pedido GET para a rota certa do teu Laravel
     // Exemplo do que falta: return this.http.???<any>(...);
-  }
+  } */
 }
